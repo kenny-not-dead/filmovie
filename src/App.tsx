c67403034ffeeaed0cc3e79dc92757b2c34c57bb
Roman <6211mail@mail.ru>
2023-04-11 22:00:46 +0300

    withRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Main from './components/main/Main';
+import Catalog from './components/catalog/Catalog';
+import Admin from './components/admin/Admin';
+import Actor from './components/actor/Actor';
+import Film from './components/fim/Film';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+        <Header/>
+            <Routes>
+                <Route path='main' element={<Main/>}/>
+                <Route path='catalog/:id' element={<Film/>}/>
+                <Route path='catalog' element={<Catalog/>}/>
+                <Route path='admin' element={<Admin />}/>
+                <Route path='actor/:id' element={<Actor/>}/>
+                <Route path="*" element={<Navigate to="main" replace />} /> 
+            </Routes>
+        <Footer/>
+        </div>
+    </BrowserRouter>
   );
 }
 
