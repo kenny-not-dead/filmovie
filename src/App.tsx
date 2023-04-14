120109dd8f908fda2e8311547c926d2e528518d4
Svork7 <svork@ya.ru>
2023-04-14 13:18:32 +0300

    layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { MainPage } from './pages/MainPage/MainPage';
-import { FilmPage } from './pages/FilmPage/FilmPage';
-import { CatalogPage } from './pages/CatalogPage/CatalogPage';
-import { AdminPage } from './pages/AdminPage/AdminPage';
-import { ActorPage } from './pages/ActorPage/ActorPage';
-import { AuthPage } from './pages/AuthPage/AuthPage';
-import { IntlProvider } from 'react-intl';
-import { LOCALES } from './i18n/locales';
-import { messages } from './i18n/messages';
-import { Header } from './components/header/Header';
-import { Footer } from './components/footer/Footer';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { MainLayout } from './layouts/MainLayout'
+import { MainPage } from './pages/MainPage/MainPage'
+import { FilmPage } from './pages/FilmPage/FilmPage'
+import { CatalogPage } from './pages/CatalogPage/CatalogPage'
+import { AdminPage } from './pages/AdminPage/AdminPage'
+import { ActorPage } from './pages/ActorPage/ActorPage'
+import { AuthPage } from './pages/AuthPage/AuthPage'
+import { IntlProvider } from 'react-intl'
+import { LOCALES } from './i18n/locales'
+import { messages } from './i18n/messages'
+import { Header } from './components/header/Header'
+import { Footer } from './components/footer/Footer'
 
 function App() {
-	const locale = LOCALES.RUSSIAN;
+  const locale = LOCALES.RUSSIAN
 
-	return (
-		<IntlProvider
-			messages={messages[locale]}
-			locale={locale}
-			defaultLocale={LOCALES.RUSSIAN}
-		>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path='/main' element={<MainPage />} />
-					<Route path='/catalog/:id' element={<FilmPage />} />
-					<Route path='/catalog' element={<CatalogPage />} />
-					<Route path='/admin' element={<AdminPage />} />
-					<Route path='/auth' element={<AuthPage />} />
-					<Route path='/actor/:id' element={<ActorPage />} />
-					<Route path='*' element={<Navigate to='main' replace />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-		</IntlProvider>
-	);
+  return (
+    <IntlProvider
+      messages={messages[locale]}
+      locale={locale}
+      defaultLocale={LOCALES.RUSSIAN}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/main" index element={<MainPage />} />
+            <Route path="/catalog/:id" element={<FilmPage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/actor/:id" element={<ActorPage />} />
+            <Route path="*" element={<Navigate to="main" replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </IntlProvider>
+  )
 }
 
-export default App;
+export default App
