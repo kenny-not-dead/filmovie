e0d918721acd882845647b6c8af76414d74e20f5
Roman <6211mail@mail.ru>
2023-04-25 21:57:58 +0300

    +adaptivemax1280

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,14 @@
-import classes from './Footer.module.scss';
-import FooterNav from './FooterNav';
-import Info from './Info';
-import SocialNetworkWrapper from './SocialNetworkWrapper';
+import classes from "./Footer.module.scss";
+import FooterNav from "./FooterNav";
+import Info from "./Info";
+import SocialNetworkWrapper from "./SocialNetworkWrapper";
 
 export function Footer() {
   return (
     <footer className={classes.container}>
-      <FooterNav/>
-      <SocialNetworkWrapper/>
-      <Info/>
+      <FooterNav />
+      <SocialNetworkWrapper />
+      <Info />
     </footer>
-  )
+  );
 }
--- src/components/footer/FooterNav.tsx
+++ src/components/footer/FooterNav.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import classes from './FooterNav.module.scss'
-import { FormattedMessage } from 'react-intl'
-import Aitem from './footernavitems/Aitem'
-import { NavLink } from 'react-router-dom'
-import { Button } from '../UI/button/Button'
-import phone from './../../static/svgs/phone.svg'
-import mail from './../../static/svgs/mail.svg'
-import logo from './../../static/svgs/logo.svg'
+import React from "react";
+import classes from "./FooterNav.module.scss";
+import { FormattedMessage } from "react-intl";
+import Aitem from "./footernavitems/Aitem";
+import { NavLink } from "react-router-dom";
+import { Button } from "../UI/button/Button";
+import phone from "./../../static/svgs/phone.svg";
+import mail from "./../../static/svgs/mail.svg";
+import logo from "./../../static/svgs/logo.svg";
 
 export default function FooterNav() {
   let item = [
     {
-      href: 'https://corp.ivi.ru/?_gl=1%2Alkzg34%2A_ga%2AMTAyOTM3NDg3NS4xNjc5ODEzNDI3%2A_ga_GETQ4387MJ%2AMTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQzMi41OS4wLjA.',
-      id: 'aboutcompani',
+      href: "https://corp.ivi.ru/?_gl=1%2Alkzg34%2A_ga%2AMTAyOTM3NDg3NS4xNjc5ODEzNDI3%2A_ga_GETQ4387MJ%2AMTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQzMi41OS4wLjA.",
+      id: "aboutcompani",
     },
     {
-      href: 'https://corp.ivi.ru/career/?_gl=1*lkzg34*_ga*MTAyOTM3NDg3NS4xNjc5ODEzNDI3*_ga_GETQ4387MJ*MTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQzMi41OS4wLjA.#career-vacancy-block',
-      id: 'vacancy',
+      href: "https://corp.ivi.ru/career/?_gl=1*lkzg34*_ga*MTAyOTM3NDg3NS4xNjc5ODEzNDI3*_ga_GETQ4387MJ*MTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQzMi41OS4wLjA.#career-vacancy-block",
+      id: "vacancy",
     },
     {
-      href: 'https://www.ivi.ru/pages/beta/',
-      id: 'beta',
+      href: "https://www.ivi.ru/pages/beta/",
+      id: "beta",
     },
     {
-      href: 'https://www.ivi.ru/info/partners',
-      id: 'partners',
+      href: "https://www.ivi.ru/info/partners",
+      id: "partners",
     },
     {
-      href: 'https://corp.ivi.ru/advertisers/?_gl=1*k75pa2*_ga*MTAyOTM3NDg3NS4xNjc5ODEzNDI3*_ga_GETQ4387MJ*MTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQ4Ny40LjAuMA..',
-      id: 'advertising',
+      href: "https://corp.ivi.ru/advertisers/?_gl=1*k75pa2*_ga*MTAyOTM3NDg3NS4xNjc5ODEzNDI3*_ga_GETQ4387MJ*MTY4MTMyMDA2NC4zLjEuMTY4MTMyOTQ4Ny40LjAuMA..",
+      id: "advertising",
     },
     {
-      href: 'https://www.ivi.ru/info/agreement',
-      id: 'agreement',
+      href: "https://www.ivi.ru/info/agreement",
+      id: "agreement",
     },
     {
-      href: 'https://www.ivi.ru/info/confidential',
-      id: 'privacy',
+      href: "https://www.ivi.ru/info/confidential",
+      id: "privacy",
     },
     {
-      href: 'https://www.ivi.ru/info/goryachaya-liniya-komplaens',
-      id: 'compliance',
+      href: "https://www.ivi.ru/info/goryachaya-liniya-komplaens",
+      id: "compliance",
     },
-  ]
+  ];
 
   return (
     <div className={classes.wrapper}>
@@ -58,10 +58,10 @@ export default function FooterNav() {
         <h4>
           <FormattedMessage id="sections" />
         </h4>
-        <NavLink to={'/main'}>
+        <NavLink to={"/"}>
           <FormattedMessage id="main" />
         </NavLink>
-        <NavLink to={'/catalog'}>
+        <NavLink to={"/filter"}>
           <FormattedMessage id="films" />
         </NavLink>
       </div>
@@ -70,7 +70,7 @@ export default function FooterNav() {
           <FormattedMessage id="support" />
         </h4>
         <p>
-          <FormattedMessage id="supportp1" /> <br />{' '}
+          <FormattedMessage id="supportp1" /> <br />{" "}
           <FormattedMessage id="supportp2" />
         </p>
         <div className={classes.btnwrapper}>
@@ -87,15 +87,17 @@ export default function FooterNav() {
           <FormattedMessage id="answer" />
         </p>
       </div>
-      <a
-        href="https://www.ivi.ru/subscribe?redirect_url=%2F"
-        className={classes.subscription}
-      >
-        <img src={logo} alt="" />
-        <p>
-          <FormattedMessage id="noadvertising" />{' '}
-        </p>
-      </a>
+      <div>
+        <a
+          href="https://www.ivi.ru/subscribe?redirect_url=%2F"
+          className={classes.subscription}
+        >
+          <img src={logo} alt="" />
+          <p>
+            <FormattedMessage id="noadvertising" />{" "}
+          </p>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
